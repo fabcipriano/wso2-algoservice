88701c27155bf6ed590a0f359a519216017308f8
Fabiano C. de Oliveira <fabianocp@algartelecom.com.br>
2018-09-23 15:04:11 -0300

    Blocked endpoint and initial handler

---
+++ wso2-newrelic/src/main/java/br/com/facio/wso2/App.java
@@ -0,0 +1,13 @@
+package br.com.facio.wso2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
